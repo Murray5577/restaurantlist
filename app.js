7dc708fade4ad17c8e145098fe64f5ff1fac4703
Murray5577 <limeyi35@gmail.com>
2024-06-06 11:16:58 +0800

    initialize sequelize has problem

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const { engine } = require('express-handlebars')
 const port = 3000
 const restaurantDatas = require('./public/jsons/restaurant.json').results
 
+const db = require('./models')
+const Restaurant = db.Restaurant
+
 app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
@@ -14,6 +17,11 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/restaurants',(req,res)=>{
+    return Restaurant.findAll()
+        .then((restaurants) => res.send({ restaurants }))
+        .catch((err)=>res.status(422).json(err))
+
+    /*
     const keyword = req.query.search
     const matchedRestaurants = keyword ? restaurantDatas.filter((rd) => Object.values(rd).some((property)=>{
         if (typeof property === 'string'){
@@ -22,13 +30,33 @@ app.get('/restaurants',(req,res)=>{
         return false
     }) 
     ) : restaurantDatas
-    res.render('index', { restaurantDatas: matchedRestaurants })
+    res.render('index', { restaurantDatas: matchedRestaurants })*/
 })
 
 app.get('/restaurants/:id',(req,res)=>{
     const id = req.params.id
     const restaurant = restaurantDatas.find((rd)=>rd.id.toString() === id)
     res.render('detail', { restaurant })
+})
+
+app.get('/restaurants/new',(req,res)=>{
+    res.send('create new restaurant')
+})
+
+app.get('/restaurants/:id/edit',(req,res)=>{
+    res.send(`edit ${req.params.id} restaurants`)
+})
+
+app.post('/restaurants',(req,res)=>{
+    res.send('post new restaurant')
+})
+
+app.put('/restaurants/:id',(res,req)=>{
+    res.send('update new restaurants')
+})
+
+app.delete('/restaurant/:id',(res,req)=>{
+    res.send('delete new restaurants')
 })
 
 app.listen(port,()=>{
